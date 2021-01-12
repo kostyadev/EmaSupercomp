06ce6459bb96ca0c53ed38048e0762ec9c76529c
kostyadev <kostya.demchenko@gmail.com>
2021-01-12 19:30:00 +0200

    first variant

--- EmaSupercomp/EmaSupercomp.cpp
+++ EmaSupercomp/EmaSupercomp.cpp
@@ -9,16 +9,117 @@ using  namespace std;
 
 vector<string> split_string(string);
 
-// Complete the twoPluses function below.
+int getMaxPlusLen(int sIdx, int cIdx, const vector<string>& grid)
+{
+	int maxLen = std::min(grid.size(), grid[0].size());
+	int retVal = 0;
+	if (grid[sIdx].at(cIdx) == 'G')
+	{
+		retVal = 1;
+	}
+	else
+	{
+		return retVal;
+	}
+
+	for (int i = 1; i <= maxLen; ++i)
+	{
+		auto r = cIdx + i;
+		auto l = cIdx - i;
+		auto d = sIdx + i;
+		auto u = sIdx - i;
+		if (r >= grid[sIdx].size() || l < 0
+			|| d >= grid.size() || u < 0)
+		{
+			break;
+		}
+
+		if (grid[sIdx][r] == 'G' && grid[sIdx][l] == 'G'
+			&& grid[u][cIdx] == 'G' && grid[d][cIdx] == 'G')
+		{
+			++retVal;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return retVal;
+}
+
+bool plusesAreOverlapped(int r1, int c1, int len1, int r2, int c2, int len2)
+{
+	auto maxLen = max(len1, len2);
+	auto dy = abs(c1 - c2);
+	auto dx = abs(r1 - r2);
+	return (dy >= 2 && dx >= (maxLen + 2)) || (dx >= 2 && dy >= (maxLen + 2));
+}
+
+int getMaxTwoPluses(int r, int c, const vector<vector<int>>& plusLens)
+{
+	const auto len = plusLens[r][c];
+	const auto area = (len - 1) * 4 + 1;
+
+	int maxArea = 0;
+	for (int ir = 0; ir < plusLens.size(); ++ir)
+	{
+		for (int ic = 0; ic < plusLens[ir].size(); ++ic)
+		{
+			const auto curLen = plusLens[ir][ic];
+			if(!plusesAreOverlapped(r, c, len, ir, ic, curLen) && curLen > 0)
+			{
+				const auto curArea = (curLen - 1) * 4 + 1;
+				if (curLen > maxArea)
+					maxArea = curArea;
+			}
+		}
+	}
+
+	return maxArea * area;
+}
+
 int twoPluses(const vector<string>& grid) 
 {
-	return 0;
+	vector<vector<int>> plusLens;
+	plusLens.reserve(grid.size());
+
+	for (int strIdx = 0; strIdx < grid.size(); ++strIdx)
+	{
+		vector<int> row(grid[strIdx].size(), 0);
+		for (int chIdx = 0; chIdx < grid[strIdx].size(); ++chIdx)
+		{
+			auto pLen = getMaxPlusLen(strIdx, chIdx, grid);
+			row[chIdx] = pLen;
+		}
+		plusLens.push_back(row);
+	}
+
+	for (const auto& r : plusLens)
+	{
+		for (const auto& c : r)
+			cout << c << " ";
+		cout << endl;
+	}
+
+	int maxVal = 0;
+	for (int ir = 0; ir < plusLens.size(); ++ir)
+	{
+		for (int ic = 0; ic < plusLens[ir].size(); ++ic)
+		{
+			auto val = getMaxTwoPluses(ir, ic, plusLens);
+			if (val > maxVal)
+				maxVal = val;
+		}
+	}
+
+	return maxVal;
 }
 
 int main()
 {
-	//ifstream fin("D:\\projects\\hacker_rank\\EmaSupercomp\\input00.txt", std::ofstream::in);
-	ifstream fin("D:\\projects\\hacker_rank\\EmaSupercomp\\input01.txt", std::ofstream::in);
+	ifstream fin("D:\\projects\\hacker_rank\\EmaSupercomp\\input00.txt", std::ofstream::in);
+	//ifstream fin("D:\\projects\\hacker_rank\\EmaSupercomp\\input01.txt", std::ofstream::in);
 
 	string nm_temp;
 	getline(fin, nm_temp);
